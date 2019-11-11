1c9b8c90f22f85cc25c83e2cee2f0f15d3f2edb3
jrco1989 <jrco1989@hotamil.com>
2019-11-11 11:28:47 -0500

    solucionando problemas

--- posts/models.py
+++ posts/models.py
@@ -8,9 +8,9 @@ from django.contrib.auth.models import User
 	#Admin=Boolean.models(default=True)"""
 class Post(models.Model):
 	#user=models.ForeignKey(User, on_delete=models.CASCADE) // para borar los post cuando se borre el usuario 
-	user=models.ForeignKey(User, on_delete=models.SET_NULL) #models.PROTEC OR models.SET_NULL//guarda los posts despues de elimiinar el usiario (portege los datos )
-	#profile=models.ForeignKey(profile, on_delete=CASCADE) 
-	profile=models.ForeignKey('user.profile',on_delete=CASCADE) #otra manera de llamar el modelo profile, que evita llamar al modelo erroneo 
+	user=models.ForeignKey(User, on_delete=models.CASCADE) #models.PROTEC OR models.SET_NULL//guarda los posts despues de elimiinar el usiario (portege los datos )
+	#profile=models.ForeignKey(profile, on_delete=models.CASCADE) 
+	profile=models.ForeignKey('user.profile',on_delete=models.CASCADE) #otra manera de llamar el modelo profile, que evita llamar al modelo erroneo 
 	title=models.CharField(max_length=89)
 	photo=models.ImageField(upload_to='posts/photos')
 	created=models.DateTimeField(auto_now_add=True)
