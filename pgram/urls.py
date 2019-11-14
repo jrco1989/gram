1e89bad4763e96b0538ecdd8fa2ccfbab6340556
jrco1989 <jrco1989@hotamil.com>
2019-11-14 16:49:34 -0500

    creación de templates

--- pgram/urls.py
+++ pgram/urls.py
@@ -4,9 +4,12 @@ from django.urls import path
 from posts import views as posts_views
 from django.conf.urls.static import static
 from django.conf import settings
+from pgram import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ('posts/', posts_views.list_posts)
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path ('posts/', posts_views.list_posts),
+    #path ('saludo', views.saludo)
+    ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)#permite mostrar archivos de con distintos formatos gráficos
+    
