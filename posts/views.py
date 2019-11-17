144a1ed7deee2c921a12d148fb6ba204d5fc85c6
jrco1989 <jrco1989@hotamil.com>
2019-11-17 15:00:38 -0500

    modificacioon de estilos

--- posts/views.py
+++ posts/views.py
@@ -8,14 +8,14 @@ from datetime import datetime
 
 posts = [
     {
-        'title': 'Mont Blanc',
+        'title': '"sistema solar"',
         'user': {
-            'name': 'Nina simone',
-            'picture': 'https://ichef.bbci.co.uk/news/660/cpsprodpb/14AAC/production/_105225648_1-1.jpg',
+            'name': 'Nina Simone',
+            'picture': 'https://picsum.photos/id/60/60/60',
         },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
         #'photo': 'https://fotografias.lasexta.com/clipping/cmsimages01/2019/06/04/3EC08BDA-7BE1-4413-A831-277039B478B9/58.jpg',
-        'photo': 'https://picsum.photos/60/60/?image=1005'
+        'photo': 'https://picsum.photos/id/800/800/903?grayscale&blur=<2></2>'
     },
     {
         'title': 'Via Láctea',
