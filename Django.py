45cef19f6a84aa4931c1868012510dccec978d48
Javier Ricardo Castiilo Ochoa <jrco1989@hotmail.com>
2021-06-28 20:09:53 -0500

    new files

---
+++ Django.py
@@ -0,0 +1,7 @@
+"***creation project ***"
+#show date formated 
+from datetime import datetime
+now = datetime.now().strftime('%b %dth, %Y -/ %H:%M')
+
+#pdb es un debbuger de python 
+import pdb; pdb.set_trace()
