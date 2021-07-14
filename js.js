f1e7ae5c276cb049b1fc813214e6262d3fed888d
Javier Ricardo Castiilo Ochoa <jrco1989@hotmail.com>
2021-07-14 00:35:06 -0500

    js

---
+++ js.js
@@ -0,0 +1,9 @@
+setTimeout(function(){ alert("Hello"); }, 3000);
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    //do work
+  });
+
+  document.getElementById('CANTIDAD_DESAGREGACION_ITEM').type = 'number';
+  document.getElementById('CANTIDAD_DESAGREGACION_ITEM').setAttribute('type','password')
+  $('#password').attr('type', 'text');
