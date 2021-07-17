209d9e1a28f6b161f3a6ee65afbb18e3cc71b607
jrco <jrco1989@hotmail.com>
2021-07-17 14:13:59 -0500

    concepts

--- main.py
+++ main.py
@@ -8,13 +8,23 @@ if __name__ == '__main__':
     args ={'status':'activate'}
     payload ={'status':'activate'}
     headers ={'Content-Type':'application/json',
-              'access-token':'fsdfsd'}#le iindicamos al servidor que estamos enviando datos en formato jsaon  
+              'access-token':'fsdfsd'}#le iindicamos al servidor que estamos enviando datos en formato json  
     response = requests.get(url2, params=args)
     response_post = requests.post(url_post, json=payload)
     response_post2 = requests.post(url_post, data=payload)
     response_post3 = requests.post(url_post, data=json.dumps(payload), headers=headers)#serializando el diccionario
     #print(response)
 
+    url_imagen ='https://s1.1zoom.me/big3/471/Painting_Art_Back_view_Photographer_575380_3840x2400.jpg'
+
+    response_img = requests.get(url_imagen, stream= True)#con strema true se realiza la peticiòn dejando la conexiòn abierta para poder descargar el contenido
+    with open('imgtest.jp', 'wb')  as file:
+        #el iter_content() toma todo el contenido del servidor y lo va descargando pocopp a poco
+        #se logra en este casi oirque ka conexiòn està abierta, este mètodo es importante para archivos pesados
+        for chunk in response_img.iter_content():
+            file.write(chunk)
+
+    response_img.close()
 if response.status_code ==200:
     #print(response.content)
     content = response.content
